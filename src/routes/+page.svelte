c8b08465208468b354c1137ca444f6fda136f033
Jelle <jelle.wiersma@windesheim.nl>
2024-04-09 14:59:35 +0200

    changed routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta name="description" content="Jelle Wiersma's showcase" />
 </svelte:head>
 
 <section>
