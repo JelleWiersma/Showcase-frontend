813a75a5688754c8645fc57186853f7aa017a1bb
Jelle <jelle.wiersma@windesheim.nl>
2024-06-05 18:29:28 +0200

    added images

--- src/routes/showcase/Components/help.svelte
+++ src/routes/showcase/Components/help.svelte
@@ -1,13 +1,16 @@
 {#if isOpen}
+<div class="background" role="presentation" on:click={close}>
     <div class="card-overlay" transition:slide="{{duration: 500, easing: backIn, axis: 'y'}}">
         <button class="close-button" on:click={close}>X</button>
         <!-- Your content here -->
     </div>
+</div>
 {/if}
 
 <script>
     import { slide } from 'svelte/transition';
     import { backIn } from 'svelte/easing';
+    import { two_of_clubs } from '$lib/images/cards/2_of_clubs.webp'
     
 
     let isOpen = false;
@@ -34,7 +37,7 @@
         box-shadow: none;
         width: 2rem;
         height: 2rem;
-        z-index: 999999;
+        
     }
 
     .card-overlay {
@@ -48,5 +51,15 @@
         display: flex;
         justify-content: center;
         align-items: center;
-    }	
+    }
+    
+    .background {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 999999;
+    }
 </style>
